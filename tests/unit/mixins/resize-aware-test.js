e36299920f1d5824b3bcef3d6bc3d2ccfff2a60d
Greg Larrenaga <greg@thedyrt.com>
2018-11-14 19:16:36 -0800

    Rewrite tests to use new ember-qunit syntax with async and await

--- tests/unit/mixins/resize-aware-test.js
+++ tests/unit/mixins/resize-aware-test.js
@@ -1,60 +1,60 @@
 import Component from '@ember/component';
+import { set } from '@ember/object';
 import ResizeAwareMixin from 'ember-resize-aware/mixins/resize-aware';
 import hbs from 'htmlbars-inline-precompile';
-import { moduleForComponent, test } from 'ember-qunit';
-import { set } from '@ember/object';
-import wait from 'ember-test-helpers/wait';
+import { render, settled } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
 
-moduleForComponent('Unit | Mixin | resize-aware', {
-  integration: true,
-  beforeEach() {
+module('Integration | Component | pretty color', function(hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(function() {
     const testContext = this;
 
-    this.register('component:resize-aware-component',
+    this.owner.register('component:resize-aware-component',
       Component.extend(ResizeAwareMixin, {
-        init() {
-          this._super(arguments);
+        init(...args) {
+          this._super(...args);
           testContext.componentInstance = this;
         }
       })
     );
-  }
-});
+  });
 
-test('didResize hook is on the object', function(assert) {
-  this.render(hbs`{{resize-aware-component}}`);
-  let resizeAwareComponent = this.componentInstance;
+  test('didResize hook is on the object', async function(assert) {
+    await render(hbs`{{resize-aware-component}}`);
+    let resizeAwareComponent = this.componentInstance;
 
-  assert.ok(resizeAwareComponent.didResize, 'didResize hook is present on the subject');
-  assert.equal(typeof resizeAwareComponent.didResize, 'function', 'didResize hook is a function');
-});
+    assert.ok(resizeAwareComponent.didResize, 'didResize hook is present on the subject');
+    assert.equal(typeof resizeAwareComponent.didResize, 'function', 'didResize hook is a function');
+  });
 
-test('it fires "didResize"  when the window is resized', function(assert) {
-  assert.expect(2);
+  test('it fires "didResize"  when the window is resized', async function(assert) {
+    assert.expect(2);
 
-  let didResizeCallCount = 0;
+    let didResizeCallCount = 0;
 
-  this.didResize = function() { didResizeCallCount++; };
-  this.debounceRate = 0;
+    this.didResize = function() { didResizeCallCount++; };
+    this.debounceRate = 0;
 
-  this.render(hbs`{{resize-aware-component didResize=didResize debounceRate=debounceRate}}`);
-  let resizeAwareComponent = this.componentInstance;
+    await render(hbs`{{resize-aware-component didResize=didResize debounceRate=debounceRate}}`);
+    let resizeAwareComponent = this.componentInstance;
 
-  let evt = new window.Event('resize');
+    let evt = new window.Event('resize');
 
-  window.dispatchEvent(evt);
+    window.dispatchEvent(evt);
 
-  return wait()
-    .then(() => {
-      assert.equal(didResizeCallCount, 1, 'didResize called 1 time on event firing');
+    await settled();
 
-      set(resizeAwareComponent, '_previousWidth', 0);
+    assert.equal(didResizeCallCount, 1, 'didResize called 1 time on event firing');
 
-      window.dispatchEvent(evt);
+    set(resizeAwareComponent, '_previousWidth', 0);
 
-      return wait();
-    })
-    .then(() => {
-      assert.equal(didResizeCallCount, 2, 'didResize called another time on event firing again');
-    });
+    window.dispatchEvent(evt);
+
+    await settled();
+
+    assert.equal(didResizeCallCount, 2, 'didResize called another time on event firing again');
+  });
 });
